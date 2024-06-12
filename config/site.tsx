6e950a93408ac91bff68f3d9de82b513f8100376
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-11 21:03:30 -0500

    feat: add site configuration file

---
+++ config/site.tsx
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'Toro',
+  url: 'https://toro.com',
+  ogImage: 'https://toro-example/og.jpg',
+  description: 'Grow your business',
+  links: {
+    twitter: 'https://twitter.com/',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
