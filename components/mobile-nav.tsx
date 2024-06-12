eb023cfe70985c7f109b86f84ba33e4f3901083d
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-12 17:45:18 -0500

    refactor: improve mobile navigation component structure and add Sheet for modal sheets

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,5 +1,68 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Menu } from 'lucide-react';
+import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
+import { services } from '@/content/services';
+import { buttonVariants } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return (
+    <div className=" md:hidden">
+      <Sheet>
+        <SheetTrigger>
+          <Menu className="h-[1.2rem] w-[1.2rem]" />
+        </SheetTrigger>
+        <SheetContent side={'left'} className="border-none">
+          <SheetHeader className="text-start">
+            <SheetTitle className="font-extrabold">Toro Marketing</SheetTitle>
+            <SheetDescription>
+              Marketing that grows your business
+            </SheetDescription>
+          </SheetHeader>
+          <nav className="flex flex-col space-y-2 pt-4 font-semibold">
+            <Separator />
+            <Link href="/services">Home</Link>
+            <Separator />
+            <Link href="/about">Services</Link>
+            {services.map((service) => (
+              <Link
+                key={service.title}
+                href="/services/"
+                className="text-muted-foreground font-normal text-sm p-2"
+              >
+                {service.title}
+              </Link>
+            ))}
+            <Separator />
+            <Link href="/about">Company</Link>
+            <Separator />
+            <Link href="/about">Work</Link>
+            <Separator />
+            <Link href="/contact">About</Link>
+            <Separator className="my-2" />
+            <Link
+              href={'/'}
+              className={buttonVariants({
+                variant: 'default',
+                size: 'default',
+              })}
+            >
+              Get Started
+              <ArrowRight className="ml-2 h-4 w-4" />
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
 };
 
 export default MobileNav;
