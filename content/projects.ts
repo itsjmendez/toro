138036fd8b484e537167b01c0551ec72823ec668
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-13 01:53:13 -0500

    refactor: add projects data to projects.ts file

---
+++ content/projects.ts
@@ -0,0 +1,22 @@
+import { Project } from '@/types/project';
+
+export const projects: Project[] = [
+  {
+    title: 'F45',
+    description:
+      'F45 Training is a global fitness community specializing in innovative, high-intensity group workouts that are fast, fun, and results-driven.',
+    tasks: ['Paid Media', 'CRM Management', 'Lead Generation'],
+  },
+  {
+    title: 'Robinson Law Firm',
+    description:
+      'With over 50 years of fighting for the rights of clients in St. Clair County and surrounding counties. The Robinson Law Firm P.C. is committed to protecting your rights and providing legal guidance.',
+    tasks: ['SEO', 'Brand Messaging', 'Website Development'],
+  },
+  {
+    title: 'JSU Stories',
+    description:
+      'Jacksonville State University (JSU) is a public university in Jacksonville, Alabama.',
+    tasks: ['Social Media', 'Video Production', 'Content Creation'],
+  },
+];
