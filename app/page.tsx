27d022938cd8d89c6e8cd90908fb1f0e8743ddb3
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-13 02:02:15 -0500

    refactor: Improve page structure and add new components for hero section, separator, card, and badge

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,62 @@
 import { HeroSection } from '@/components/hero';
+import { Separator } from '@/components/ui/separator';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { projects } from '@/content/projects';
+import { Badge } from '@/components/ui/badge';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <div>
+        <div className="container my-20">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Add growth to your business
+          </h1>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            As your marketing growth partner, we use data, digital marketing,
+            creative and messaging to attract, engage, convert and retain
+            customers.
+          </p>
+        </div>
+        <Separator />
+      </div>
+      <div>
+        <div className="container my-20">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Our recent projects
+          </h1>
+          <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-4 [&:not(:first-child)]:mt-6">
+            {projects.map((project) => (
+              <div key={project.title}>
+                <Card className="w-full">
+                  <CardHeader>
+                    <CardTitle>{project.title}</CardTitle>
+                    <CardDescription>{project.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <ul>
+                      {project.tasks?.map((task) => (
+                        <li key={task}>
+                          <Badge variant="outline">{task}</Badge>
+                        </li>
+                      ))}
+                    </ul>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Separator />
+      </div>
     </main>
   );
 }
