8491134fa69673cdddf916959a85497ab7a10a76
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-18 05:32:35 -0500

    refactor: Update HeroSection component structure and add Separator

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,34 +6,32 @@ import { Separator } from './ui/separator';
 export function HeroSection() {
   return (
     <div>
-      <div className="container py-20">
-        <div className="md:text-center ">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-6xl">
-            Toro Marketing
-          </h1>
-          <h2 className="pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-muted-foreground">
-            Marketing That Grows Your Business
-          </h2>
-          <div className="mt-4 gap-4 flex md:items-center md:justify-center">
-            <Link
-              href={'/'}
-              className={buttonVariants({
-                variant: 'default',
-                size: 'default',
-              })}
-            >
-              Our Services
-            </Link>
-            <Link
-              href={'/'}
-              className={buttonVariants({
-                variant: 'outline',
-                size: 'default',
-              })}
-            >
-              Free Audit <ArrowRight className="ml-2 h-4 w-4" />
-            </Link>
-          </div>
+      <div className="container my-20 flex flex-col gap-4">
+        <h1 className="font-semibold tracking-tight text-5xl sm:text-8xl md:text-9xl lg:text-10xl uppercase">
+          Toro Marketing
+        </h1>
+        <h3 className="text-3xl font-normal tracking-tight first:mt-0 text-muted-foreground">
+          Grow Your Business
+        </h3>
+        <div className="gap-4 flex md:items-center">
+          <Link
+            href={'/'}
+            className={buttonVariants({
+              variant: 'default',
+              size: 'default',
+            })}
+          >
+            Our Services
+          </Link>
+          <Link
+            href={'/'}
+            className={buttonVariants({
+              variant: 'outline',
+              size: 'default',
+            })}
+          >
+            Free Audit <ArrowRight className="ml-2 h-4 w-4 text-primary" />
+          </Link>
         </div>
       </div>
       <Separator />
