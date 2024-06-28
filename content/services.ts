c14e5cbf9b6c7feeab553294aa576384720b55c9
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-28 03:29:43 -0500

    refactor: Update services page with new service offerings

--- content/services.ts
+++ content/services.ts
@@ -5,30 +5,35 @@ export const services: NavItem[] = [
     title: 'Multicultural Marketing',
     href: '/services/multicultural-marketing',
     description:
-      'Looking to reach Spanish-speaking audiences? We provide authentic messaging that goes beyond direct translation, helping your brand resonate with Spanish-speaking customers.',
+      'We provide authentic messaging that goes beyond direct translation, helping your brand resonate with Spanish-speaking customers.',
+    catchyLabel: 'Looking to reach Spanish-speaking audiences?',
   },
   {
     title: 'Web Services',
     href: '/services/web-services',
     description:
-      'Not happy with your website? We provide design strategy, web development and page optimization.',
+      'We provide design strategy, web development and page optimization.',
+    catchyLabel: 'Not happy with your website? ',
   },
   {
     title: 'Digital Marketing',
     href: '/services/digital-marketing',
     description:
-      'Not getting enough reach? Increase online visibility with social media, video, audio, and blogging across organic and paid media.',
+      'Increase online visibility with social media, video, audio, and blogging across organic and paid media.',
+    catchyLabel: 'Not getting enough reach?',
   },
   {
     title: 'Branding',
     href: '/services/branding',
     description:
-      'Need help telling your story? Position and establish your brand in the eyes of your consumers with compelling storytelling and visual identity.',
+      'Position and establish your brand in the eyes of your consumers with compelling storytelling and visual identity.',
+    catchyLabel: 'Need help telling your story?',
   },
   {
     title: 'Lifecycle',
     href: '/services/lifecycle',
     description:
-      'Need to boost revenue with your CRM? We provide strategies to help you cross-sell, retain and make your customers loyal.',
+      'We provide strategies to help you cross-sell, retain and make your customers loyal.',
+    catchyLabel: 'Need to boost revenue with your CRM?',
   },
 ];
