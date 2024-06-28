d6e17102e68ba168d761dbb830f0e1526655d3cb
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-28 03:29:51 -0500

    refactor: Update hero section layout and image source

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,24 +8,24 @@ export const HeroSection = () => {
     <div className="relative h-[calc(100vh-3.5rem)] overflow-hidden">
       <Image
         fill={true}
-        src="/hero-bg-02.jpg"
+        src="/hero-bg-01.jpg"
         alt="Background"
         quality={100}
         priority
         style={{ objectFit: 'cover' }}
       />
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="container relative z-10 flex flex-col gap-4 justify-center h-full">
+      <div className="absolute inset-0 bg-black opacity-50" />
+      <div className="container relative z-10 flex flex-col gap-8 md:gap-12 justify-center h-full">
         <h1 className="font-semibold tracking-tight text-5xl md:text-7xl text-white uppercase max-w-[700px]">
           Elevate Your Brand with Toro Marketing
         </h1>
-        <h2 className="text-3xl md:text-4xl font-normal tracking-tight text-white">
+        <h2 className="text-2xl md:text-4xl font-normal tracking-tight text-white">
           Turn Your Vision into Reality
         </h2>
         <div className="flex-col md:flex-row gap-4 flex md:items-center">
           <Link
             href={'/'}
-            className={`border-none ${buttonVariants({
+            className={`border-none rounded-none ${buttonVariants({
               variant: 'default',
               size: 'lg',
             })}`}
@@ -34,7 +34,7 @@ export const HeroSection = () => {
           </Link>
           <Link
             href={'/'}
-            className={`border-none ${buttonVariants({
+            className={`border-none rounded-none ${buttonVariants({
               variant: 'outline',
               size: 'lg',
             })}`}
