dd76ad0182e7baad6ccb3d104bc66c9aaa923749
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-29 14:51:34 -0500

    refactor: Update Cta component to use button prop with label and variant

--- components/cta.tsx
+++ components/cta.tsx
@@ -4,22 +4,27 @@ import { CtaProps } from '@/types/cta';
 
 // TODO: Add functionality to the CTA component
 
-const Cta = ({ header, subheader, btnText }: CtaProps) => {
+const Cta = ({ header, subheader, button }: CtaProps) => {
   return (
     <div className="container">
-      <div className="relative py-10 md:py-24 border-x hover:dark:bg-white hover:dark:text-black hover:bg-black hover:text-white">
-        <div className=" flex flex-col gap-4">
-          <h3 className="text-4xl md:text-6xl uppercase text-center">
+      <div className="relative py-10 md:py-24 border-x bg-primary">
+        <div className="container flex flex-col gap-4 justify-center items-center">
+          <h3 className="text-2xl md:text-4xl uppercase text-center text-primary-foreground">
             {header}
           </h3>
-          {subheader && <p className="text-xl text-center">{subheader}</p>}
-          {btnText && (
+          {subheader && (
+            <p className="text-xl text-center text-primary-foreground">
+              {subheader}
+            </p>
+          )}
+          {button && (
             <Button
-              variant="default"
-              size="lg"
-              className="border-none rounded-none"
+              variant={button.variant}
+              size={button.size}
+              className="rounded-none bg-primary-foreground text-primary hover:bg-primary hover:text-primary-foreground hover:shadow-2xl hover:border hover:border-primary-foreground"
             >
-              {btnText} <ArrowRight className="ml-2 h-4 w-4 text-primary" />
+              {button.label}
+              <ArrowRight className="ml-2 h-4 w-4" />
             </Button>
           )}
         </div>
