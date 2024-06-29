3da82647c4913df8a3914b4f811449451d0c774c
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-28 21:52:42 -0500

    refactor: Remove unused components header and footer from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
-import SiteHeader from '@/components/site-header';
-import SiteFooter from '@/components/site-footer';
 
 export const metadata: Metadata = {
   title: 'Toro Consulting',
@@ -26,9 +24,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <SiteHeader />
           {children}
-          <SiteFooter />
         </ThemeProvider>
       </body>
     </html>
