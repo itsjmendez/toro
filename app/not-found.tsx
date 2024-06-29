4ba308e17b87b5bdac3b4c0b45d4f74edd47d0e0
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-28 23:17:35 -0500

    refactor: Update NotFound component to use simplified CSS class names

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="container">
-      <div className="relative h-[calc(100vh-10.1rem)] overflow-hidden border-x justify-center items-center flex flex-col gap-4">
-        <h2>Uh oh! Page Not Found 😐</h2>
+      <div className="relative h-svh overflow-hidden justify-center items-center flex flex-col gap-4">
+        <h2>Uh oh! Page Not Found 🐂</h2>
         <Link className="underline font-bold" href="/">
           Return Home
         </Link>
