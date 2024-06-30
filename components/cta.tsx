fbd3a9cb31e4a315c6f4b4e2d5e33413f32e7b37
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-30 18:12:09 -0500

    refactor: Update Cta component to remove unnecessary container div

--- components/cta.tsx
+++ components/cta.tsx
@@ -6,28 +6,26 @@ import { CtaProps } from '@/types/cta';
 
 const Cta = ({ header, subheader, button }: CtaProps) => {
   return (
-    <div className="container">
-      <div className="relative py-10 md:py-24 border-x bg-primary">
-        <div className="container flex flex-col gap-4 justify-center items-center">
-          <h3 className="text-2xl md:text-4xl uppercase text-center text-primary-foreground">
-            {header}
-          </h3>
-          {subheader && (
-            <p className="text-xl text-center text-primary-foreground">
-              {subheader}
-            </p>
-          )}
-          {button && (
-            <Button
-              variant={button.variant}
-              size={button.size}
-              className="rounded-none bg-primary-foreground text-primary hover:bg-primary hover:text-primary-foreground hover:shadow-2xl hover:border hover:border-primary-foreground"
-            >
-              {button.label}
-              <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-          )}
-        </div>
+    <div className="relative py-10 md:py-24 border-x bg-primary">
+      <div className="container flex flex-col gap-4 justify-center items-center">
+        <h3 className="text-2xl md:text-4xl uppercase text-center text-primary-foreground">
+          {header}
+        </h3>
+        {subheader && (
+          <p className="text-xl text-center text-primary-foreground">
+            {subheader}
+          </p>
+        )}
+        {button && (
+          <Button
+            variant={button.variant}
+            size={button.size}
+            className="rounded-none bg-primary-foreground text-primary hover:bg-primary hover:text-primary-foreground hover:shadow-2xl hover:border hover:border-primary-foreground"
+          >
+            {button.label}
+            <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        )}
       </div>
     </div>
   );
