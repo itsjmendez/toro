1a0b9f01bbe1cfe6b6e544746e6c827a24b820ec
Josemanuel Mendez <jmocontactme@gmail.com>
2024-06-30 18:55:23 -0500

    refactor: Update hero section styling and content

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,56 +1,58 @@
 import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
-// import Image from 'next/image';
 import ContainerEdgeCross from './ui/container-edge-cross';
 
 export const HeroSection = () => {
   return (
-    <div className="relative">
-      <ContainerEdgeCross tr={true} tl={true} bl={true} br={true}>
-        <div className="container relative h-[calc(100vh-3.5rem)] overflow-hidden">
-          {/* <Image
-          fill={true}
-          src="/hero-bg-01.jpg"
-          alt="Background"
-          quality={80}
-          priority
-          style={{ objectFit: 'cover' }}
-          /> */}
-          {/* <div className="absolute inset-0 bg-black opacity-50" /> */}
-          <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-          <div className="absolute top-0 left-0  z-[-2] h-screen w-screen  bg-[radial-gradient(ellipse_100%_80%_at_50%_0%,rgba(120,119,198,0.3),rgba(255,255,255,0))] dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
-          <div className="container relative flex flex-col gap-8 md:gap-12 justify-center h-full">
-            <h1 className="font-semibold tracking-tight text-4xl md:text-7xl uppercase max-w-[700px]">
-              Unlock Your Competitive Edge
-            </h1>
-            <h2 className="text-2xl md:text-4xl font-normal tracking-tight ">
-              Marketing to Scale Your Business
+    <ContainerEdgeCross>
+      <div className="relative p-4 md:p-6 overflow-hidden">
+        <div className="relative flex flex-col gap-8 justify-center h-full">
+          <h1 className="font-semibold tracking-tight text-4xl md:text-5xl lg:text-6xl uppercase">
+            Elevate your brand
+          </h1>
+          <div>
+            <h2 className="tracking-tight text-2xl md:text-3xl font-semibold mb-1">
+              Get modern strategies.
             </h2>
-            <div className="flex-col md:flex-row gap-4 flex md:items-center">
-              <Link
-                href={'/services'}
-                className={`border-none rounded-none ${buttonVariants({
-                  variant: 'default',
-                  size: 'lg',
-                })}`}
-              >
-                Our Solutions
-              </Link>
-              <Link
-                href={'/'}
-                className={` rounded-none ${buttonVariants({
-                  variant: 'outline',
-                  size: 'lg',
-                })}`}
-              >
-                Free Consultation
-                <ArrowRight className="ml-2 h-4 w-4 text-primary" />
-              </Link>
-            </div>
+            <p className="text-xl text-muted-foreground">
+              Sell more with integrated marketing.
+            </p>
+          </div>
+          <div className="flex flex-col md:flex-row gap-2 md:items-center">
+            <Link
+              href="/services"
+              className={`border-none rounded-none ${buttonVariants({
+                variant: 'default',
+                size: 'lg',
+              })}`}
+            >
+              Our Solutions
+            </Link>
+            <Link
+              href="/"
+              className={`rounded-none ${buttonVariants({
+                variant: 'outline',
+                size: 'lg',
+              })}`}
+            >
+              Get a Free Consultation
+              <ArrowRight className="ml-2 h-4 w-4 text-primary" />
+            </Link>
           </div>
         </div>
-      </ContainerEdgeCross>
-    </div>
+      </div>
+      <div className="relative">
+        <video
+          className="object-cover h-[70svh] w-full"
+          loop
+          autoPlay={true}
+          preload="none"
+          muted
+        >
+          <source src="./hero-1.mp4" type="video/mp4" />
+        </video>
+      </div>
+    </ContainerEdgeCross>
   );
 };
