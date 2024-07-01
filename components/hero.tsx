cb14a66a6e3ec8672d71a4bb69a9821cd5d4d634
Josemanuel Mendez <jmocontactme@gmail.com>
2024-07-01 16:13:20 -0500

    refactor: Update hero section styling and content

--- components/hero.tsx
+++ components/hero.tsx
@@ -9,33 +9,31 @@ export const HeroSection = () => {
     <ContainerEdgeCross>
       <div className="relative p-4 md:p-6 h-[calc(100svh-8svh)]">
         <div className="relative flex flex-col gap-8 h-full justify-center md:items-center text-center ">
-          <h1 className="font-semibold tracking-tight text-4xl md:text-5xl text-muted-foreground">
-            The <span className="text-primary">marketing</span> your company
-            deserves.
+          <h1 className="font-bold tracking-tight text-4xl md:text-5xl">
+            The marketing your company deserves.
           </h1>
-          <h2 className="tracking-tight text-xl md:text-3xl mb-1">
-            No More Missed Opportunities,
-            <br />
-            Only Gains.
+          <h2 className="tracking-tight text-xl md:text-3xl font-semibold">
+            No More Missed Opportunities, Only Gains.
           </h2>
-          <div className=" max-w-96">
+          <div className="md:px-0 max-w-96">
             <TextGenerateEffect
-              words=" Drive growth with our integrated marketing solutions, fusing technology, branding, and customer journeys for powerful results."
+              words="Drive growth with our integrated marketing solutions, fusing technology, branding, and customer journeys for powerful results."
               className="text-base md:text-xl text-muted-foreground tracking-tight"
             />
           </div>
-
-          <div className=" flex flex-col gap-4 ">
-            <Link
-              href="/"
-              className={`${buttonVariants({
-                variant: 'round',
-                size: 'default',
-              })}`}
-            >
-              Free Consultation
-              <ArrowRight className="ml-1 h-4 w-4" />
-            </Link>
+          <div className="px-12 flex flex-col gap-4">
+            <ContainerEdgeCross trc={true} blc={true}>
+              <Link
+                href="/"
+                className={`w-full ${buttonVariants({
+                  variant: 'default',
+                  size: 'default',
+                })}`}
+              >
+                Free Consultation
+                <ArrowRight className="ml-1 h-4 w-4" />
+              </Link>
+            </ContainerEdgeCross>
             <Link
               href="/services"
               className={`underline ${buttonVariants({
