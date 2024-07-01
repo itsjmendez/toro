fdc9e493dfba0a56eda895fcbdc822f765c23516
Josemanuel Mendez <jmocontactme@gmail.com>
2024-07-01 13:37:17 -0500

    refactor: Update globals.css to round button corners

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -14,6 +14,8 @@ const buttonVariants = cva(
         destructive: 'bg-destructive text-destructive-foreground',
         outline:
           'border border-primary bg-background hover:bg-primary hover:text-primary-foreground',
+        round:
+          'bg-primary text-primary-foreground hover:bg-background hover:text-foreground hover:ring-1 hover:ring-primary rounded-3xl',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
